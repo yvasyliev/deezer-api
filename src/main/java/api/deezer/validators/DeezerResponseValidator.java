d46b2fff58ca2c459d8071c10fe45f036bd58f06
Yevhen Vasyliev <ye.vasyliev@gmail.com>
2022-10-14 13:39:31 +0300

    Fixed #12

--- src/main/java/api/deezer/validators/DeezerResponseValidator.java
+++ src/main/java/api/deezer/validators/DeezerResponseValidator.java
@@ -11,13 +11,26 @@ import java.util.function.Predicate;
  */
 public class DeezerResponseValidator implements Predicate<String> {
     /**
+     * <code>wrong code</code> body content.
+     */
+    private static final String WRONG_CODE = "wrong code";
+
+    /**
      * Converts Deezer response to {@link JsonElement}.
      */
     private static final Converter<String, JsonElement> JSON_ELEMENT_CONVERTER = new JsonElementConverter();
 
     @Override
     public boolean test(String responseBody) {
+        return !isWrongCode(responseBody) && !hasErrorField(responseBody);
+    }
+
+    private boolean isWrongCode(String responseBody) {
+        return WRONG_CODE.equals(responseBody);
+    }
+
+    private boolean hasErrorField(String responseBody) {
         JsonElement jsonElement = JSON_ELEMENT_CONVERTER.convert(responseBody);
-        return !jsonElement.isJsonObject() || !jsonElement.getAsJsonObject().has("error");
+        return jsonElement.isJsonObject() && jsonElement.getAsJsonObject().has("error");
     }
 }
