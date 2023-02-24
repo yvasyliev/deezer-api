b5f6d976d965f3e1364b6f4039e744f6ad5dda89
Yevhen Vasyliev <ye.vasyliev@gmail.com>
2023-02-24 12:41:45 +0200

    fixed infinite deserialization recursion

--- src/main/java/api/deezer/deserializers/TrackDataDeserializer.java
+++ src/main/java/api/deezer/deserializers/TrackDataDeserializer.java
@@ -1,19 +1,29 @@
 package api.deezer.deserializers;
 
+import api.deezer.objects.Track;
 import api.deezer.objects.data.TrackData;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
+import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
+import java.util.List;
 
 public class TrackDataDeserializer implements JsonDeserializer<TrackData> {
     @Override
     public TrackData deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
-        boolean isFalse = jsonElement.isJsonPrimitive()
-                && jsonElement.getAsJsonPrimitive().isBoolean()
-                && jsonElement.getAsBoolean();
-        return isFalse ? null : jsonDeserializationContext.deserialize(jsonElement, type);
+        if (jsonElement.isJsonPrimitive() && jsonElement.getAsJsonPrimitive().isBoolean() && !jsonElement.getAsBoolean()) {
+            return null;
+        }
+
+        JsonObject jsonObject = jsonElement.getAsJsonObject();
+        TrackData trackData = new TrackData();
+        trackData.setNext(jsonObject.get("next").getAsString());
+        trackData.setTotal(jsonObject.get("total").getAsInt());
+        trackData.setData(jsonDeserializationContext.deserialize(jsonObject.get("data"), TypeToken.getParameterized(List.class, Track.class).getType()));
+        return trackData;
     }
 }
